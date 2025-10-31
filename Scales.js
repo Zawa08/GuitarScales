2bd4dacad544d5258529ff33dcf85d8d428255d6
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2025-10-31 20:18:01 +0100

    scales.js with major scales

--- Scales.js
+++ Scales.js
@@ -1,61 +1,57 @@
 export const Scales = {
-    Major: {},
+    Major: {
+        C: {
+            notes: ["C", "D", "E", "F", "G", "A", "B"],
+            img: "Images\Scales\Major\CMajor.svg"
+        },
+        CSharp: {
+            notes: ["C#", "D#", "E#", "F#", "G#", "A#", "B#"]
+        },
+        DFlat: {
+            notes: ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"]
+        },
+        D: {
+            notes: ["D", "E", "F#", "G", "A", "B", "C#"]
+        },
+        DSharp: {
+            notes: ["D#", "E#", "F##", "G#", "A#", "B#", "C##"]
+        },
+        EFlat: {
+            notes: ["Eb", "F", "G", "Ab", "Bb", "C", "D"]
+        },
+        E: {
+            notes: ["E", "F#", "G#", "A", "B", "C#", "D#"]
+        },
+        F: {
+            notes: ["F", "G", "A", "Bb", "C", "D", "E"]
+        },
+        FSharp: {
+            notes: ["F#", "G#", "A#", "B", "C#", "D#", "E#"]
+        },
+        GFlat: {
+            notes: ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"]
+        },
+        G: {
+            notes: ["G", "A", "B", "C", "D", "E", "F#"]
+        },
+        GSharp: {
+            notes: ["G#", "A#", "B#", "C#", "D#", "E#", "F##"]
+        },
+        AFlat: {
+            notes: ["Ab", "Bb", "C", "Db", "Eb", "F", "G"]
+        },
+        AMajor: {
+            notes: ["A", "B", "C#", "D", "E", "F#", "G#"]
+        },
+        ASharp: {
+            notes: ["A#", "B#", "C##", "D#", "E#", "F##", "G##"]
+        },
+        BFlat: {
+            notes: ["Bb", "C", "D", "Eb", "F", "G", "A"]
+        },
+        B: {
+            notes: ["B", "C#", "D#", "E", "F#", "G#", "A#"]
+        }
+    },
     Minor: {}
 }
-
-switch (scale) {
-    case "CMajor":
-        return ["C", "D", "E", "F", "G", "A", "B"];
-
-    case "CSharpMajor":
-        return ["C#", "D#", "E#", "F#", "G#", "A#", "B#"];
-
-    case "DFlatMajor":
-        return ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"];
-
-    case "DMajor":
-        return ["D", "E", "F#", "G", "A", "B", "C#"];
-
-    case "DSharpMajor":
-        return ["D#", "E#", "F##", "G#", "A#", "B#", "C##"];
-
-    case "EFlatMajor":
-        return ["Eb", "F", "G", "Ab", "Bb", "C", "D"];
-
-    case "EMajor":
-        return ["E", "F#", "G#", "A", "B", "C#", "D#"];
-
-    case "FMajor":
-        return ["F", "G", "A", "Bb", "C", "D", "E"];
-
-    case "FSharpMajor":
-        return ["F#", "G#", "A#", "B", "C#", "D#", "E#"];
-
-    case "GFlatMajor":
-        return ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"];
-
-    case "GMajor":
-        return ["G", "A", "B", "C", "D", "E", "F#"];
-
-    case "GSharpMajor":
-        return ["G#", "A#", "B#", "C#", "D#", "E#", "F##"];
-
-    case "AFlatMajor":
-        return ["Ab", "Bb", "C", "Db", "Eb", "F", "G"];
-
-    case "AMajor":
-        return ["A", "B", "C#", "D", "E", "F#", "G#"];
-
-    case "ASharpMajor":
-        return ["A#", "B#", "C##", "D#", "E#", "F##", "G##"];
-
-    case "BFlatMajor":
-        return ["Bb", "C", "D", "Eb", "F", "G", "A"];
-
-    case "BMajor":
-        return ["B", "C#", "D#", "E", "F#", "G#", "A#"];
-
-    default:
-        return ["", "", "", "", "", "", ""];
-}
-
