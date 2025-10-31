221563659fcf78d2780d24668adff886ab4df0d7
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2025-10-31 19:42:09 +0100

    adding external objects

--- LearnAScale.js
+++ LearnAScale.js
@@ -1,64 +1,11 @@
+import {Scales} from "Scales.js";
+
 function getScale() {
-    let scale = document.getElementById('scale-select').value
-    switch (scale) {
-        case "CMajor":
-            return ["C", "D", "E", "F", "G", "A", "B"];
-
-        case "CSharpMajor":
-            return ["C#", "D#", "E#", "F#", "G#", "A#", "B#"];
-
-        case "DFlatMajor":
-            return ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"];
-
-        case "DMajor":
-            return ["D", "E", "F#", "G", "A", "B", "C#"];
-
-        case "DSharpMajor":
-            return ["D#", "E#", "F##", "G#", "A#", "B#", "C##"];
-
-        case "EFlatMajor":
-            return ["Eb", "F", "G", "Ab", "Bb", "C", "D"];
-
-        case "EMajor":
-            return ["E", "F#", "G#", "A", "B", "C#", "D#"];
-
-        case "FMajor":
-            return ["F", "G", "A", "Bb", "C", "D", "E"];
-
-        case "FSharpMajor":
-            return ["F#", "G#", "A#", "B", "C#", "D#", "E#"];
-
-        case "GFlatMajor":
-            return ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"];
-
-        case "GMajor":
-            return ["G", "A", "B", "C", "D", "E", "F#"];
-
-        case "GSharpMajor":
-            return ["G#", "A#", "B#", "C#", "D#", "E#", "F##"];
-
-        case "AFlatMajor":
-            return ["Ab", "Bb", "C", "Db", "Eb", "F", "G"];
-
-        case "AMajor":
-            return ["A", "B", "C#", "D", "E", "F#", "G#"];
-
-        case "ASharpMajor":
-            return ["A#", "B#", "C##", "D#", "E#", "F##", "G##"];
-
-        case "BFlatMajor":
-            return ["Bb", "C", "D", "Eb", "F", "G", "A"];
-
-        case "BMajor":
-            return ["B", "C#", "D#", "E", "F#", "G#", "A#"];
-
-        default:
-            return ["", "", "", "", "", "", ""];
-    }
+    const scale = document.getElementById('scale-select').value
 }
 
 function DisplayScale() {
-    let scale = getScale();
+    const scale = getScale();
     let noteList = "<ul class='scale-list'>";
     scale.forEach(note => {
         noteList += "<li>" + note + "</li>";
---
+++ Scales.js
@@ -0,0 +1,61 @@
+export const Scales = {
+    Major: {},
+    Minor: {}
+}
+
+switch (scale) {
+    case "CMajor":
+        return ["C", "D", "E", "F", "G", "A", "B"];
+
+    case "CSharpMajor":
+        return ["C#", "D#", "E#", "F#", "G#", "A#", "B#"];
+
+    case "DFlatMajor":
+        return ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"];
+
+    case "DMajor":
+        return ["D", "E", "F#", "G", "A", "B", "C#"];
+
+    case "DSharpMajor":
+        return ["D#", "E#", "F##", "G#", "A#", "B#", "C##"];
+
+    case "EFlatMajor":
+        return ["Eb", "F", "G", "Ab", "Bb", "C", "D"];
+
+    case "EMajor":
+        return ["E", "F#", "G#", "A", "B", "C#", "D#"];
+
+    case "FMajor":
+        return ["F", "G", "A", "Bb", "C", "D", "E"];
+
+    case "FSharpMajor":
+        return ["F#", "G#", "A#", "B", "C#", "D#", "E#"];
+
+    case "GFlatMajor":
+        return ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"];
+
+    case "GMajor":
+        return ["G", "A", "B", "C", "D", "E", "F#"];
+
+    case "GSharpMajor":
+        return ["G#", "A#", "B#", "C#", "D#", "E#", "F##"];
+
+    case "AFlatMajor":
+        return ["Ab", "Bb", "C", "Db", "Eb", "F", "G"];
+
+    case "AMajor":
+        return ["A", "B", "C#", "D", "E", "F#", "G#"];
+
+    case "ASharpMajor":
+        return ["A#", "B#", "C##", "D#", "E#", "F##", "G##"];
+
+    case "BFlatMajor":
+        return ["Bb", "C", "D", "Eb", "F", "G", "A"];
+
+    case "BMajor":
+        return ["B", "C#", "D#", "E", "F#", "G#", "A#"];
+
+    default:
+        return ["", "", "", "", "", "", ""];
+}
+
