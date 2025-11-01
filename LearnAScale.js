c4b6137e86fdd5453e06ec350eca9b1aefded84d
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2025-11-01 15:02:13 +0100

    Update LearnAScale.js

--- LearnAScale.js
+++ LearnAScale.js
@@ -3,17 +3,17 @@ import { Scales } from "./Scales.js";
 const ScaleSelect = document.getElementById('scale-select');
 
 function DisplayScale() {
-    const Scale = Scales["Major"][ScaleSelect.value]["notes"];
-
-    function createList() {
-        let noteList = "<ol class='scale-list'>";
-        Scale.forEach(note => {
-            noteList += "<li>" + note + "</li>";
-        });
-        noteList += "</ol>";
-        return noteList;
-    }
     ScaleSelect.addEventListener("change", () => {
+        const Scale = Scales["Major"][ScaleSelect.value]["notes"];
+
+        function createList() {
+            let noteList = "<ol class='scale-list'>";
+            Scale.forEach(note => {
+                noteList += "<li>" + note + "</li>";
+            });
+            noteList += "</ol>";
+            return noteList;
+        }
         document.getElementById("scales").innerHTML = createList();
     });
 }
