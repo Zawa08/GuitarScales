6a8c854477c540e5ad34d54f47e70682d8087f92
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2025-11-03 19:46:50 +0100

    Added PracticeChords.html

---
+++ LearnChords.js
@@ -0,0 +1,6 @@
+import { Scales } from "./Scales.js";
+
+const EasyButton = document.getElementById("easy");
+const MediumButton = document.getElementById("medium");
+const HardButton = document.getElementById("hard");
+
