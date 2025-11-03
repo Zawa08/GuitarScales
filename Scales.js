c18ba00bd4c857b2547cea41d3cd6c44df92e299
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2025-11-03 18:42:35 +0100

    updated Blues 12

--- Scales.js
+++ Scales.js
@@ -158,6 +158,6 @@ export const Preset = {
         intervals: [0, 5, 3, 4]
     },
     BluesTwelve: {
-        intervals: [0, 0, 0, 0, 3, 3, 0, 0, 4, 3, 0, 4]
+        intervals: [7, 7, 7, 7, 10, 10, 7, 7, 11, 10, 7, 11]
     }
 }
