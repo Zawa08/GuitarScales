565b473535fee38e4fefebb508c1137bffe82de5
Zawa08 <73878883+Zawa08@users.noreply.github.com>
2026-06-01 20:27:36 +0200

    Refactored ChordProgression.js

--- ChordProgression.js
+++ ChordProgression.js
@@ -1,142 +1,122 @@
 import { Keys, ChordDiagrams, Preset } from "./Scales.js";
 import { createDiagram } from "./ChordMaker.js";
 
-const KeySelect = document.getElementById("key-select");
+// --- DOM ELEMENTY ---
+const keySelect = document.getElementById("key-select");
+const chordList = document.getElementById("chord-list");
+const checkboxSeven = document.getElementById("checkbox-seven");
+const removeBtn = document.getElementById("remove-btn");
+const removeAllBtn = document.getElementById("remove-all-btn");
 
-let SevenChecked = false;
-let Key = structuredClone(Keys["Major"][KeySelect.value]["chords"]);
-let ChordIntervals = [];
+// --- STAV APLIKACE (STATE) ---
+// Udržuje čistě pole čísel (0-6 = běžné akordy, 7-13 = sedmičkové)
+let chordIntervals = [];
+// Tónina jako rychlé lokální pole
+let currentKeyChords = [...Keys["Major"][keySelect.value]["chords"]];
 
-function getChordNameAndPositions(interval) {
-  let position = [];
-  let name = "";
-  if (interval > 6) {
-    interval -= 7;
-    SevenChecked = true;
+// --- ČISTÁ LOGIKA (PURE FUNCTIONS) ---
+function getChordData(intervalValue) {
+  // Funkce už neovlivňuje žádné globální proměnné
+  const isSeventh = intervalValue > 6;
+  const normalizedInterval = isSeventh ? intervalValue - 7 : intervalValue;
+  const baseChord = currentKeyChords[normalizedInterval];
+
+  let type = "major";
+  let suffix = "";
+
+  if ([1, 2, 5].includes(normalizedInterval)) {
+    type = isSeventh ? "minorSeventh" : "minor";
+    suffix = isSeventh ? "m7" : "m";
+  } else if (normalizedInterval === 6) {
+    type = isSeventh ? "diminishedSeventh" : "diminished";
+    suffix = isSeventh ? "dim7" : "dim";
   } else {
-    SevenChecked = false;
+    type = isSeventh ? "seventh" : "major";
+    suffix = isSeventh ? "7" : "";
   }
-  if (interval === 0 || interval === 3 || interval === 4) {
-    name = Key[interval] + (SevenChecked ? "7" : "");
-    position = ChordDiagrams[Key[interval]][SevenChecked ? "seventh" : "major"];
-  } else if (interval === 1 || interval === 2 || interval === 5) {
-    name = Key[interval] + (SevenChecked ? "m7" : "m");
-    position =
-      ChordDiagrams[Key[interval]][SevenChecked ? "minorSeventh" : "minor"];
-  } else {
-    name = Key[interval] + (SevenChecked ? "dim7" : "dim");
-    position =
-      ChordDiagrams[Key[interval]][
-        SevenChecked ? "diminishedSeventh" : "diminished"
-      ];
-  }
-  return [name, position];
+
+  return {
+    name: baseChord + suffix,
+    positions: ChordDiagrams[baseChord][type],
+  };
 }
 
-function getChord(interval) {
-  const button = document.getElementById(interval);
-  button.addEventListener("click", () => {
-    const chordInterval = SevenChecked ? interval + 7 : interval;
-    ChordIntervals.push(chordInterval);
-    const chordNameAndPosition = getChordNameAndPositions(chordInterval);
-    createChord(chordNameAndPosition[0], chordNameAndPosition[1]);
-  });
-}
-
-function getPreset(preset) {
-  const button = document.getElementById(preset);
-  button.addEventListener("click", () => {
-    const ChordPreset = structuredClone(Preset[preset]["intervals"]);
-    ChordIntervals = ChordPreset;
-    updateList();
-  });
-}
-
-function isChecked(element) {
-  const checkBox = document.getElementById(element);
-  checkBox.addEventListener("change", () => {
-    if (SevenChecked) {
-      SevenChecked = false;
-    } else {
-      SevenChecked = true;
-    }
-  });
-}
-
-function removeChordButton() {
-  const button = document.getElementById("remove-btn");
-  const chordItems = document.getElementsByClassName("chord-item");
-  button.addEventListener("click", () => {
-    [...chordItems].at(-1).remove();
-  });
-}
-
-function removeAllChordsButton() {
-  const button = document.getElementById("remove-all-btn");
-  const chordItems = document.getElementsByClassName("chord-item");
-  button.addEventListener("click", () => {
-    [...chordItems].forEach((chordItem) => {
-      chordItem.remove();
-      ChordIntervals = [];
-    });
-  });
-}
-
-function removeAllChords() {
-  const chordItems = document.getElementsByClassName("chord-item");
-  [...chordItems].forEach((chordItem) => {
-    chordItem.remove();
-  });
-}
-
-function createChord(chordName, fingerPositions) {
-  const chordList = document.getElementById("chord-list");
+// --- VYKRESLOVÁNÍ (DOM MANIPULACE) ---
+function renderChord(chordName, fingerPositions) {
+  if (!chordList) return;
   const li = document.createElement("li");
-  li.setAttribute("class", "chord-item");
+  li.className = "chord-item";
+
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   createDiagram(svg, chordName, fingerPositions);
+
   li.appendChild(svg);
   chordList.appendChild(li);
 }
 
-function updateList() {
-  removeAllChords();
-  ChordIntervals.forEach((interval) => {
-    const chordNameAndPositions = getChordNameAndPositions(interval);
-    createChord(chordNameAndPositions[0], chordNameAndPositions[1]);
+function reRenderAllChords() {
+  // Nejčistší způsob vymazání prvků rodiče (rychlejší než innerHTML = "")
+  chordList.replaceChildren();
+
+  chordIntervals.forEach((intervalValue) => {
+    const { name, positions } = getChordData(intervalValue);
+    renderChord(name, positions);
   });
 }
 
-function intervalButtons() {
+// --- INICIALIZACE A EVENT LISTENERY ---
+function setupApp() {
+  // 1. Změna tóniny (zaktualizuje stav a VŠE překreslí)
+  keySelect.addEventListener("change", () => {
+    currentKeyChords = [...Keys["Major"][keySelect.value]["chords"]];
+    reRenderAllChords();
+  });
+
+  // 2. Tlačítka pro jednotlivé intervaly (I, ii, iii...)
   for (let i = 0; i <= 6; i++) {
-    getChord(i);
+    const btn = document.getElementById(i.toString());
+    if (btn) {
+      btn.addEventListener("click", () => {
+        // Kontrola aktuálního stavu switche PŘÍMO z elementu
+        const finalInterval = checkboxSeven.checked ? i + 7 : i;
+
+        chordIntervals.push(finalInterval);
+        const { name, positions } = getChordData(finalInterval);
+        renderChord(name, positions);
+      });
+    }
+  }
+
+  // 3. Preset tlačítka
+  ["Major", "MajorMinor", "BluesTwelve"].forEach((presetId) => {
+    const btn = document.getElementById(presetId);
+    if (btn) {
+      btn.addEventListener("click", () => {
+        // Zkopírování pole do stavu a kompletní překreslení
+        chordIntervals = [...Preset[presetId]["intervals"]];
+        reRenderAllChords();
+      });
+    }
+  });
+
+  // 4. Odstranění POSLEDNÍHO akordu (Oprava zombie bugu)
+  if (removeBtn) {
+    removeBtn.addEventListener("click", () => {
+      if (chordIntervals.length > 0) {
+        chordIntervals.pop(); // Nejdřív odstranit záznam ze stavové proměnné!
+        chordList.lastElementChild?.remove(); // Pak smazat vizuál z DOMu
+      }
+    });
+  }
+
+  // 5. Odstranění VŠECH akordů
+  if (removeAllBtn) {
+    removeAllBtn.addEventListener("click", () => {
+      chordIntervals = []; // Čistě vyprázdnit stav
+      chordList.replaceChildren(); // Efektivně vyčistit vizuál
+    });
   }
 }
 
-function presetButtons() {
-  getPreset("Major");
-  getPreset("MajorMinor");
-  getPreset("BluesTwelve");
-}
-
-function switches() {
-  isChecked("checkbox-seven");
-}
-
-function changeScale() {
-  KeySelect.addEventListener("change", () => {
-    Key = structuredClone(Keys["Major"][KeySelect.value]["chords"]);
-    updateList();
-  });
-}
-
-function displayChords() {
-  intervalButtons();
-  presetButtons();
-  switches();
-  removeChordButton();
-  removeAllChordsButton();
-  changeScale();
-}
-
-displayChords();
+// Spuštění
+setupApp();
